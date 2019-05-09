62f164bbc7dc77d3d614f4df701220d0fc4c3663
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2019-05-09 12:12:07 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # terraform-aws-ssm-parameter-store
 Terraform module to fetch ARNs of secrets from SSM parameter store
+
+Usage:
+
+```hcl
+module "metabase_encryption_secret_key" {
+  source = "github.com/comtravo/terraform-aws-ssm-parameter-store"
+
+  parameter = "MB_ENCRYPTION_SECRET_KEY"
+}
+
+```
---
+++ main.tf
@@ -0,0 +1,19 @@
+variable "parameter" {
+  type = "string"
+  description = "parameter for which the ARN needs to be fetched"
+}
+
+
+locals {
+  prefix = "${upper(terraform.workspace)}_"
+}
+
+data "aws_ssm_parameter" "parameter" {
+  name = "${local.prefix}${var.parameter}"
+  with_decryption = false
+}
+
+output arn {
+  description = "SSM parameter ARN"
+  value       = "${data.aws_ssm_parameter.parameter.arn}"
+}
