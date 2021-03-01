18fb8840e76845f6a951e96da95e5c1710f3d6cb
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-03-01 18:08:31 +0100

    TF 0.13 upgrade

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,18 +4,6 @@ variable "secret_name" {
 }
 
 provider "aws" {
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  access_key                  = "This is not an actual access key."
-  secret_key                  = "This is not an actual secret key."
-
-  endpoints {
-    iam = "http://localstack:4593"
-    ssm = "http://localstack:4583"
-    sts = "http://localstack:4592"
-  }
 }
 
 module "fetch_secret_arn" {
